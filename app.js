88988e6c5030ec871e823f983e4f669f8f76dce1
OBPSG <vbeasupergamer@gmail.com>
2022-02-11 10:28:28 -0600

    Finished lab

--- app.js
+++ app.js
@@ -2,36 +2,47 @@ var friends = ["John", "Jane", "Mark", "Katie", "Smith"];
 
 //Function that outputs the chorus for the 99 Lines of Code Song for a single friend
 function Sing99LinesSong(singer) {
+  let container = document.createElement("div");
+  container.classList.add("friend");
+  document.body.appendChild(container);
+
+  let friendHeader = document.createElement("h3");
+  friendHeader.appendChild(document.createTextNode(singer));
+  container.appendChild(friendHeader);
+
   for (let i = 99; i > 0; i--) {
+    let line = "";
     if (i == 1) {
-      console.log(
-        "Only 1 line of code in the file, only 1 more line of code; " +
-          singer +
-          " strikes one out, clears it all out, no more lines of code in the file!"
-      );
+      line = "Only 1 line of code in the file, only 1 more line of code; " +
+        singer +
+        " strikes one out, clears it all out, no more lines of code in the file!";
     } else if (i == 2) {
-      console.log(
-        "2 lines of code in the file, 2 lines of code; " +
-          singer +
-          " strikes one out, clears it all out, only 1 line of code in the file!"
-      );
+      line = "2 lines of code in the file, 2 lines of code; " +
+        singer +
+        " strikes one out, clears it all out, only 1 line of code in the file!";
     } else {
-      console.log(
+      line = i +
+        " lines of code in the file, " +
         i +
-          " lines of code in the file, " +
-          i +
-          " lines of code; " +
-          singer +
-          " strikes one out, clears it all out, " +
-          (i - 1) +
-          " lines of code in the file!"
-      );
+        " lines of code; " +
+        singer +
+        " strikes one out, clears it all out, " +
+        (i - 1) +
+        " lines of code in the file!"
     }
+    let chorusLine = document.createElement("p");
+    chorusLine.classList.add("song-line");
+    chorusLine.appendChild(document.createTextNode(line));
+    container.appendChild(chorusLine);
   }
 }
 
-//Loop over the friends array calling the Sing99LinesSong function
-for (let i = 0; i < friends.length; i++) {
-  console.log(friends[i].toUpperCase() + ":");
-  Sing99LinesSong(friends[i]);
-}
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+  //Loop over the friends array calling the Sing99LinesSong function
+  for (let i = 0; i < friends.length; i++) {
+    Sing99LinesSong(friends[i]);
+  }
+});
+
+
